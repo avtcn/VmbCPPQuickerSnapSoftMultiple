bf4d87a0ec031798f53050be1943e7d8e8b359a8
joege <geyijun@gmail.com>
2020-12-26 11:55:24 +0800

    trim  move_vimba_cpp_path.sh

--- Build/VS2010/move_vimba_cpp_path.sh
+++ Build/VS2010/move_vimba_cpp_path.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-vimba_path='C:\Users\Public\Documents\Allied Vision\Vimba_4.0\VimbaCPP_Examples\'
-vimba_path2='C:/Users/Public/Documents/Allied Vision/Vimba_4.0/VimbaCPP_Examples/'
+# vimba_path='C:\Users\Public\Documents\Allied Vision\Vimba_4.0\VimbaCPP_Examples\'
+# echo $vimba_path
 
-vimba_example_path=`find /mnt/c/Users/Public  -type d -name "VimbaCPP_Examples" | head -n 1`
-echo $vimba_example_path
+# vimba_example_path=`find /mnt/c/Users/Public  -type d -name "VimbaCPP_Examples" | head -n 1`
+# echo $vimba_example_path
 
 source_str='..\..\..\..\Build\VS2010\'
 echo $source_str
-echo $vimba_path
+
 
 # sed --debug -i  "s/'$source_str'/'$vimba_path'\Build\VS2010\/g" AsynchronousGrabMFC2.sln
 
@@ -20,4 +20,5 @@ cat temp1.vcxproj
 # cat temp2.vcxproj
 cp temp1.vcxproj AsynchronousGrabConsole.vcxproj
 
+rm -rf temp1.vcxproj
 
